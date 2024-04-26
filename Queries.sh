bfd55fce51f398d13b32e71c51511b811394b734
Thrashbang <159561923+Thrashbang@users.noreply.github.com>
2024-04-25 18:07:56 -0600

    Update Queries.sh

--- Queries.sh
+++ Queries.sh
@@ -26,18 +26,16 @@ echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo "$($PSQL "SELECT count(winner_goals) FROM games WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-MY_QUERY = $($PSQL "SELECT team_id FROM games WHERE year=2018 AND round='Final'")
-echo "$($PSQL "SELECT name FROM teams WHERE team_id='$MY_QUERY'")"
-<<COMMENTS
+echo "$($PSQL "SELECT name FROM teams WHERE team_id=$($PSQL "SELECT winner_id FROM games WHERE year=2018 AND round='Final'")")"
+
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo
+echo "$($PSQL "SELECT * FROM teams WHERE team_id=$($PSQL "SELECT winner_id FROM games WHERE year=2014 AND round='Eighth-Final'")")"
 
-echo -e "\nList of unique winning team names in the whole data set:"
-echo
+#echo -e "\nList of unique winning team names in the whole data set:"
+#echo
 
-echo -e "\nYear and team name of all the champions:"
-echo
+#echo -e "\nYear and team name of all the champions:"
+#echo
 
-echo -e "\nList of teams that start with 'Co':"
-echo
-COMMENTS
+#echo -e "\nList of teams that start with 'Co':"
+#echo
